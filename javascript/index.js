b613469d4a2ea2b6c84899f5770ee521bddd63c7
TLaloo <Tricia_Laloo@quadra.ca>
2023-01-24 14:05:23 -0500

    Added Toronto, CSS Changes

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,17 @@
 function updateTime() {
+  // Toronto
+  let torontoElement = document.querySelector("#toronto");
+  if (torontoElement) {
+    let torontoDateElement = torontoElement.querySelector(".date");
+    let torontoTimeElement = torontoElement.querySelector(".time");
+    let torontoTime = moment().tz("Canada/Toronto");
+
+    torontoDateElement.innerHTML = torontoTime.format("MMMM Do YYYY");
+    torontoTimeElement.innerHTML = torontoTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+
   // Los Angeles
   let losAngelesElement = document.querySelector("#los-angeles");
   if (losAngelesElement) {
